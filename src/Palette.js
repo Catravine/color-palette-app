25437371aa6d96a64af5969779fb2d072d3744be
Catravine <caroline@carolinecourtney.com>
2023-02-05 14:01:41 -0500

    styled the slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import "./Palette.css";
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import "./Palette.css";
 
 
 class Palette extends Component {
@@ -25,13 +25,23 @@ class Palette extends Component {
 
     return (
       <div className="Palette">
-        <Slider 
-          defaultValue={this.state.level} 
-          min={100} 
-          max={900} 
-          step={100} 
-          onChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider 
+            defaultValue={level} 
+            min={100} 
+            max={900} 
+            step={100} 
+            onChange={this.changeLevel}
+            railStyle={{height: '8px'}}
+            trackStyle={[{backgroundColor: 'transparent'}]}
+            handleStyle={[{
+              backgroundColor: 'green',
+              outline: 'none',
+              border: '2px solid green',
+              boxShadow: 'none'
+            }]}
+          />
+        </div>
         {/* navbar goes here */}
         <div className="Palette-colors">
           {colorBoxes}
