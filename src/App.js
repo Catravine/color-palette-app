b933e55bed9510a9f584ac0a024b693281e8fd7c
Catravine <caroline@carolinecourtney.com>
2023-02-11 16:11:10 -0500

    setup routes, broke app for now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from "./seedColors";
-import {generatePalette} from './colorHelpers';
+import { generatePalette } from './colorHelpers';
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>LIST OF PALETTES</h1>} />
+      <Route path="/palette/:id" element={<h1>individual palletes</h1>} />
+    </Routes>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
