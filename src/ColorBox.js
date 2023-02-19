cbdf4b32e7234ec491f313120b4e511a94ef6ded
Catravine <caroline@carolinecourtney.com>
2023-02-19 16:31:55 -0500

    adjust text colors based on liminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom'
 import "./ColorBox.css"
+import chroma from 'chroma-js';
 
 class ColorBox extends Component {
   constructor(props) {
@@ -10,32 +11,37 @@ class ColorBox extends Component {
     this.changeCopyState = this.changeCopyState.bind(this);
   }
 
-changeCopyState() {
-  this.setState({copied: true}, () => {
-    setTimeout(() => this.setState({copied: false}), 1500);
-  })
-}
+  changeCopyState() {
+    this.setState({copied: true}, () => {
+      setTimeout(() => this.setState({copied: false}), 1500);
+    })
+  }
 
   render() {
     const {name, background, moreUrl, showLink } = this.props;
     const {copied} = this.state;
+    const isDarkColor = chroma(background).luminance() <= 0.08;
+    const isLightColor = chroma(background).luminance() >= 0.7;
+
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{background}} className="ColorBox">
           <div style={{background}} className={`copy-overlay ${copied && "show"}`} />
           <div className={`copy-msg ${copied && "show"}`}>
             <h1>Copied!</h1>
-            <p>{this.props.background}</p>
+            <p className={isLightColor && 'dark-text'}>{this.props.background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span>{name}</span>
+              <span className={isDarkColor && 'light-text'}>
+                {name}
+              </span>
             </div>
-            <button className="copy-button">Copy</button>
+            <button className={`copy-button ${isLightColor && 'dark-text'}`}>COPY</button>
           </div>
           {showLink && (
             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-              <span className="see-more">More</span>
+              <span className={`see-more ${isLightColor && 'dark-text'}`}>MORE</span>
             </Link>
           )}
         </div>
